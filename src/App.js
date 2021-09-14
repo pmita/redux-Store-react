348d84fb3a50c34fb3251b5b945a03c532d17381
pans mita <panos@panss-Mac-mini.local>
2021-09-14 15:18:04 +0100

    routing pages added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+//import everything react router related
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+//importing our routing components/pages
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Products from './pages/Products';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>Welcome to redux store practice</h1>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/cart' component={Cart} />
+        <Route path='/products' component={Products} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
